0f7c41dbe856e51839c141597d003d1f38300795
AndonyNS <andony91@gmail.com>
2014-09-05 10:50:48 -0600

    Base para el proyecto

--- Buses/app/src/androidTest/java/com/example/busdevelop/buses/ApplicationTest.java
+++ Buses/app/src/androidTest/java/com/example/busdevelop/buses/ApplicationTest.java
@@ -1,4 +1,4 @@
-package com.example.andony.bus;
+package com.example.busdevelop.buses;
 
 import android.app.Application;
 import android.test.ApplicationTestCase;
--- Buses/app/src/main/java/com/example/busdevelop/buses/MainActivity.java
+++ Buses/app/src/main/java/com/example/busdevelop/buses/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.andony.bus;
+package com.example.busdevelop.buses;
 
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
