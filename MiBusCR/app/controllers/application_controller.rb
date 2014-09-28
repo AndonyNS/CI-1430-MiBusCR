9b61bd2f3adf89e9107ef7c0f6d76f5aae0174d0
manuCR <manuelarroyoportilla@gmail.com>
2014-09-27 22:51:48 -0600

    Agregado generacion del token y la recuperacion del mismo por el momento no existe la autenticacion

--- MiBusCR/app/controllers/application_controller.rb
+++ MiBusCR/app/controllers/application_controller.rb
@@ -3,4 +3,20 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   # protect_from_forgery with: :exception
   force_ssl
+
+    private
+    def restrict_access
+      unless restrict_access_by_params
+        render json: {message: 'Invalid API Token'}, status: 401
+        return
+      end
+      @current_user
+    end
+
+    def restrict_access_by_params
+      @current_user = User.find_by_token(params[:token])
+
+      return true if @current_user
+    end
+
 end
--- MiBusCR/app/models/user.rb
+++ MiBusCR/app/models/user.rb
@@ -1,4 +1,13 @@
 class User < ActiveRecord::Base
  has_many :ruta_user
  has_many :ruta, through: :ruta_user
+
+ before_create :generate_token
+
+  private
+    def generate_token
+      begin
+        self.token = SecureRandom.hex.to_s
+      end while self.class.exists?(token: token)
+    end
 end
