71b9e5ac02899f2dfe8b0328aafbe888ae1f7431
esr91 <esr5.91@gmail.com>
2014-10-03 18:28:33 -0600

    Update and rename Fragment1.java to FragmentEditarCuenta.java

--- Buses/app/src/main/java/com/example/busdevelop/buses/FragmentEditarCuenta.java
+++ Buses/app/src/main/java/com/example/busdevelop/buses/FragmentEditarCuenta.java
@@ -9,13 +9,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class Fragment1 extends Fragment {
+public class FragmentEditarCuenta extends Fragment {
 
     @Override
     public View onCreateView(
             LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
 
-        return inflater.inflate(R.layout.fragment_1, container, false);
+        return inflater.inflate(R.layout.fragment_editar_cuenta, container, false);
     }
 }
