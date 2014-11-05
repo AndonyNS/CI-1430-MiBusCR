fb49fd775cf86e0be068ab53626c9c1ca41ac178
manuCR <manuelarroyoportilla@gmail.com>
2014-11-05 00:57:53 -0600

    arreglado un problema con el api a la hora de agregar usuarios via api se podia repetir email

--- MiBusCR/app/models/user.rb
+++ MiBusCR/app/models/user.rb
@@ -3,6 +3,9 @@ class User < ActiveRecord::Base
  has_many :ruta, through: :ruta_user
 
  before_create :generate_token
+ EMAIL_REGEX = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+ validates :nombre, :presence => true, :uniqueness => true, :length => { :in => 3..20 }
+ validates :email, :presence => true, :uniqueness => true, :format => EMAIL_REGEX
 
   private
     def generate_token
