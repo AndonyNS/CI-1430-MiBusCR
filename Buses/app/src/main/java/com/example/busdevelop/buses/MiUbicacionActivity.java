9b71d6ad78e26d0d93de6b2df9e7590aeb724f60
alexis10 <jossuegross@gmail.com>
2014-11-15 00:43:27 -0600

    Agregado listview a Ubicacion Activity, cambios diseño de los listview

--- Buses/app/src/main/java/com/example/busdevelop/buses/MiUbicacionActivity.java
+++ Buses/app/src/main/java/com/example/busdevelop/buses/MiUbicacionActivity.java
@@ -1,14 +1,19 @@
 package com.example.busdevelop.buses;
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.os.AsyncTask;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -19,16 +24,26 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class MiUbicacionActivity extends ActionBarActivity implements LocationListener{
     GoogleMap mMapa;
     private LocationManager mLocationManager;
     private Marker mMarcadorUpdate;
+    private Usuario mUsuario;
+    private ListView mListViewRutasCercanas;
+    private List<Ruta> mListaRutas;
+    private ArrayList<Ruta> mListaRutasCercanas;
+    private ListViewAdapter mAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mi_ubicacion);
 
+        getRutas();
         try {
             if (mMapa == null) {
                 // soportado en ultimas apis
@@ -116,6 +131,85 @@ public class MiUbicacionActivity extends ActionBarActivity implements LocationLi
         CameraUpdate zoom = CameraUpdateFactory.zoomTo(zoomDistance);
         mMapa.moveCamera(ubicacion);
         mMapa.animateCamera(zoom);
+    }
+
+    /**
+     * Obtener los datos del usuario y pedir las rutas
+     */
+    private void getRutas(){
+        mUsuario = new Usuario();
+        SharedPreferences sharedPref = getSharedPreferences("MyPrefsFile", 0);
+        mUsuario.setEmail(sharedPref.getString("UserEmail", ""));
+        mUsuario.setEncrypted_password(sharedPref.getString("UserPass", ""));
+
+        new HttpAsyncTaskToken(this).execute();
+    }
+
+    /**
+     * Obtener el token para poder consultar rutas
+     */
+    private class HttpAsyncTaskToken extends AsyncTask<Void, Void, String> {
+        Activity mActivity;
+        private HttpAsyncTaskToken(Activity activity){
+            this.mActivity = activity;
+        }
+
+        @Override
+        protected String doInBackground(Void...params) {
+            return mUsuario.obtenerToken();
+        }
+        /**
+         * metodo que se ejecuta después de obtener la respuesta
+         * al request post del token
+         * @param resultado
+         */
+        @Override
+        protected void onPostExecute(String resultado) {
+            try {
+                mUsuario.guardarTokenId(resultado);
+                // una vez obtenido el token se pide las rutas
+                new HttpAsyncTask(mActivity).execute();
+            } catch(IllegalArgumentException i){
+                Log.e("Error de argumento",""+i.getMessage());
+            }
+        }
+    }
+
+    private class HttpAsyncTask extends AsyncTask<String, Void, String> {
+        Activity mActivity;
+        private HttpAsyncTask(Activity activity){
+            this.mActivity = activity;
+        }
+
+        @Override
+        protected String doInBackground(String... urls) {
+            ManejadorRutas manejador = ManejadorRutas.getInstancia(mUsuario.getToken());
+            mListaRutas = manejador.getListaRutas();
+            return "Rutas Obtenidas!";
+        }
+
+        /**
+         * metodo que se ejecuta después de obtener la respuesta
+         * al request get
+         * @param result
+         */
+        @Override
+        protected void onPostExecute(String result) {
+
+            try{
+                Toast.makeText(getBaseContext(), result, Toast.LENGTH_LONG).show();
+                // createListView();
+                // TODO: Calcular las rutas cercanas
+                mListViewRutasCercanas = (ListView) findViewById(R.id.listaDeRutas);
+                // Pasar las rutas al  ListViewAdapter
+                mAdapter = new ListViewAdapter(mActivity,(ArrayList) mListaRutas);
+
+                // enlazar el adaptador con el listView
+                mListViewRutasCercanas.setAdapter(mAdapter);
+            } catch(IllegalArgumentException i){
+                Log.e("Error de argumento",""+i.getMessage());
+            }
+        }
     }
 
 }
